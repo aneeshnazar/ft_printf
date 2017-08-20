2d890978a36fd58e9e6b34f9f6e43ade047e9dee
Aneesh Nazar <anazar@e1z2r9p40.42.us.org>
2017-08-19 21:47:01 -0700

    %p

--- printf/handle_hash.c
+++ printf/handle_hash.c
@@ -6,7 +6,7 @@
 /*   By: anazar <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/13 21:25:50 by anazar            #+#    #+#             */
-/*   Updated: 2017/08/19 20:39:04 by anazar           ###   ########.fr       */
+/*   Updated: 2017/08/19 21:46:39 by anazar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 void	handle_hash(t_able *table)
 {
-	if (table->format.f_hash && table->data.u != 0)
+	if (table->format.f_hash)
 	{
-		if (ft_is_in(table->format.type, "oO"))
+		if (ft_is_in(table->format.type, "oO") && table->data.u != 0)
 			table->tmp = ft_strjoinfree("0", table->tmp);
-		else if (table->format.type == 'x' || table->format.type == 'p')
+		else if ((table->format.type == 'x' && table->data.u != 0)
+			|| (table->format.type == 'p'))
 			table->tmp = ft_strjoinfree("0x", table->tmp);
-		else if (table->format.type == 'X')
+		else if (table->format.type == 'X' && table->data.u != 0)
 			table->tmp = ft_strjoinfree("0X", table->tmp);
 	}
 }

e749cc7b888bf71f2ebd1ba0d5890732c97ae66a
Aneesh Nazar <anazar@e1z2r9p40.42.us.org>
2017-08-19 22:01:16 -0700

    plus bug

--- printf/check_plus.c
+++ printf/check_plus.c
@@ -6,7 +6,7 @@
 /*   By: anazar <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/03 16:29:22 by anazar            #+#    #+#             */
-/*   Updated: 2017/08/13 23:58:07 by anazar           ###   ########.fr       */
+/*   Updated: 2017/08/19 22:01:02 by anazar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,17 @@
 int		check_plus(t_able *table)
 {
 	if (table->format.f_plus)
+	{
 		if (ft_is_in(table->format.type, "di"))
+		{
 			if (table->data.d >= 0)
 				return (1);
+		}
+		else if (table->format.type == 'D')
+		{
+			if (table->data.ld >= 0)
+				return (1);
+		}
+	}
 	return (0);
 }
